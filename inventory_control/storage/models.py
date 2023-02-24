b76424c6326170d937ff82a0c0879cbbec72f959
omen-sn <124202301+omen-sn@users.noreply.github.com>
2023-02-24 23:50:33 +0200

    Update models.py

--- inventory_control/storage/models.py
+++ inventory_control/storage/models.py
@@ -15,15 +15,9 @@ class Goods(models.Model):
     def __str__(self):
         return self.title
 
-    '''def get_absolute_url(self):
-        return reverse('post', kwargs={'post_slug': self.slug})'''
-
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True, verbose_name='Категорія')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     def __str__(self):
         return self.name
-
-    '''def get_absolute_url(self):
-        return reverse('category', kwargs={'cat_slug': self.slug})'''
