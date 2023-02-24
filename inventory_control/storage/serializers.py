4cec2bfedd40c1dc0e0d6732181a2a45ac242d33
omen-sn <124202301+omen-sn@users.noreply.github.com>
2023-02-24 23:54:23 +0200

    Update serializers.py

--- inventory_control/storage/serializers.py
+++ inventory_control/storage/serializers.py
@@ -1,13 +1,7 @@
-import io
-
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-
 from .models import *
 
 class GoodsSerializer(serializers.ModelSerializer):
-    #user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     user = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Goods
