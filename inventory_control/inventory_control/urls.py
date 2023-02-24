15022fc9e81e32469206628a8b60c2dcd511b4cd
omen-sn <124202301+omen-sn@users.noreply.github.com>
2023-02-24 23:49:27 +0200

    Update urls.py

--- inventory_control/inventory_control/urls.py
+++ inventory_control/inventory_control/urls.py
@@ -1,41 +1,4 @@
-"""inventory_control URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include, re_path
-from rest_framework import routers
-
-from storage.views import *
-
-'''
-router = routers.SimpleRouter()
-router.register(r'goods', GoodsViewSet, basename='goods')
-'''
-'''
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/auth/', include('rest_framework.urls')),
-    path('api/v1/goods/', GoodsAPIList.as_view()),
-    path('api/v1/goods/<int:pk>/', GoodsAPIUpdate.as_view()),
-    path('api/v1/goodsdelete/<int:pk>/', GoodsAPIDestroy.as_view()),
-    path('api/v1/auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
-]
-'''
-
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
